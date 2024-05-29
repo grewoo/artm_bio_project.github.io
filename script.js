1de5a097079ec7780b64a5b45015767c936096f8
grewoo <aakt08@gmail.com>
2024-05-30 00:05:32 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// Modal functionality
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = 'block';
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = 'none';
+}
+
+// Close modal when clicking outside of it
+window.onclick = function(event) {
+    const modals = document.getElementsByClassName('modal');
+    for (let i = 0; i < modals.length; i++) {
+        if (event.target == modals[i]) {
+            modals[i].style.display = 'none';
+        }
+    }
+};
+
+// Registration form functionality
+document.getElementById('registerForm').onsubmit = function(event) {
+    event.preventDefault();
+    alert('Реєстрація пройшла успішно!');
+    closeModal('modalForm');
+};
